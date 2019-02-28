3b570d22fe7b1423df5a63f6e0ad80feca348315
Adrien <seikay83@gmail.com>
2019-02-28 20:31:16 +0100

    Ajout projet test unity

---
+++ Back2L Experiment/Assets/Scripts/Command Pattern Test/MoveRight.cs
@@ -0,0 +1,20 @@
+﻿public class MoveRight : ICommand
+{
+    Character character;
+
+    public MoveRight(Character character)
+    {
+        this.character = character;
+    }
+
+    public void Execute()
+    {
+        character.MoveHorizontal(1);
+    }
+
+    public void Undo()
+    {
+        
+    }
+}
+
